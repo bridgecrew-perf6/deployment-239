b11e5dec6566db1b908f8e6aa5781b8ae5f8f7c2
saikumar <sai999694@gmail.com>
2022-02-18 14:13:26 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,23 @@
+
+
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def HomePage(request,template_name="homepage.html"):
+
+    if request.method =="POST":
+        try:
+            User.objects.create(
+                                username=request.POST.get('fname')+" " + request.POST.get('lname'),
+                                first_name=request.POST.get('fname'),
+                                last_name=request.POST.get('lname'),
+                                email=request.POST.get('email'),
+                                password=request.POST.get('password')
+            )
+            messages.success(request,"User created successfully",fail_silently=True)
+        except Exception as e :
+            print("Error creating")
+            pass
+
+    return render(request, template_name,locals())
